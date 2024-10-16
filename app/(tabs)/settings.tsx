c20f0dbb9bb1dc899646c3eff15bcf25d68f8010
Brian <gamerfunky78@gmail.com>
2024-10-16 17:49:30 +0300

    added settings.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -2,15 +2,18 @@ import { Image, StyleSheet, Platform } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { ScrollView } from 'react-native-gesture-handler';
-
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 
 
 export default function HomeScreen() {
   return (
       <ThemedView style={styles.stepContainer}>
-        <ThemedText  style={styles.text} >SETTINGS</ThemedText>
+        <ThemedText style={styles.text}>
+        <TabBarIcon name={'settings'} style={styles.icons}/>
+          GIVEAWAYS
+        </ThemedText>
         <ScrollView contentContainerStyle={styles.scrollViewContent}>
-          
+          <ThemedText style={styles.text}>Like The app? Rate us on the google or appstore.</ThemedText>
         </ScrollView>
       </ThemedView>
     
@@ -38,5 +41,8 @@ const styles = StyleSheet.create({
     paddingBottom: 20, // Space at the bottom of the scrollable content
     gap: 10,
     backgroundColor:'#1b2838',
+  },
+  icons: {
+    marginTop: 5,
   },
 });
